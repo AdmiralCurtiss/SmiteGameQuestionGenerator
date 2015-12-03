92ea9b9a4c2bbef08449da796ff1b840e25e1bb1
Tim Miller <innerlogic4321@gmail.com>
2015-12-03 09:34:24 -0500

    Add timer, music, sfx, other stuff

--- quiz.js
+++ quiz.js
@@ -1,3 +1,4 @@
+var isCalled = false;
 var $quiz = {
     loadJSON: function(path, callback) {
         var xobj = new XMLHttpRequest();
@@ -10,45 +11,53 @@ var $quiz = {
         };
         xobj.send(null);  
     },
-    loadGame : function(path) {
-        $quiz.loadJSON(path, function(response) {
-            var actual_JSON = JSON.parse(response);
-            $('#startGame').fadeOut();
-            $('#pointLayout').fadeIn();
-            var table = document.getElementById('pointTable').getElementsByTagName('tbody')[0];
-            var tableRow = 0;
-            for (var i = 0; i < actual_JSON.length; i++) {
-                var cellNumber = i % 4;
-                if (i % 4 == 0) {
-                    var row = table.insertRow(tableRow);
-                    tableRow++;
+    loadGame: function (path) {
+        if (!isCalled) {
+            isCalled = true;
+            $quiz.loadJSON(path, function (response) {
+                var actual_JSON = JSON.parse(response);
+                $('#startGame').fadeOut();
+                $('#pointLayout').fadeIn();
+                var table = document.getElementById('pointTable').getElementsByTagName('tbody')[0];
+                var tableRow = 0;
+                for (var i = 0; i < actual_JSON.length; i++) {
+                    var cellNumber = i % 4;
+                    if (i % 4 == 0) {
+                        var row = table.insertRow(tableRow);
+                        tableRow++;
+                    }
+
+                    var cell = row.insertCell(cellNumber);
+                    cell.innerText = actual_JSON[i].GameTitle;
+                    cell.setAttribute("class", "game-select-question");
+                    cell.setAttribute("data-question-number", i);
                 }
 
-                var cell = row.insertCell(cellNumber);
-                cell.innerText = actual_JSON[i].GameTitle;
-                cell.setAttribute("class", "game-select-question");
-                cell.setAttribute("data-question-number", i);
-            }
+                $(".answer").click(function () {
+                    var correct = $(this).attr("data-correct") === "true";
+                    $quiz.isCorrect(correct, false);
+                    cdreset();
+                });
 
-            $(".answer").click(function () {
-                var correct = $(this).attr("data-correct") === "true";
-
-                $('[data-correct="false"]').addClass("show-wrong");
-                $('[data-correct="true"]').addClass("show-correct");
-                if (correct) {
-                    $('.right-answer-choice').fadeIn();
-                } else {
-                    $('.wrong-answer-choice').fadeIn();
-                }
-
-                setTimeout(function () { $quiz.setAnswerClearAndReturn(correct) }, 2000);
+                $(".game-select-question").click(function () {
+                    var questionNumber = $(this).attr("data-question-number");
+                    $quiz.loadQuestion(questionNumber, actual_JSON[questionNumber]);
+                });
             });
+        }
+    },
+    isCorrect: function (correct, timeup) {
+        $('[data-correct="false"]').addClass("show-wrong");
+        $('[data-correct="true"]').addClass("show-correct");
+        if (correct) {
+            $('#right-sound').clone()[0].play();
+            $('.right-answer-choice').fadeIn();
+        } else {
+            $('#wrong-sound').clone()[0].play();
+            $('.wrong-answer-choice').fadeIn();
+        }
 
-            $(".game-select-question").click(function () {
-                var questionNumber = $(this).attr("data-question-number");
-                $quiz.loadQuestion(questionNumber, actual_JSON[questionNumber]);
-            });
-        });
+        setTimeout(function () { $quiz.setAnswerClearAndReturn(correct) }, 2000);
     },
     loadQuestion: function (questionNumber, question) {
         $('#pointTable').fadeOut();
